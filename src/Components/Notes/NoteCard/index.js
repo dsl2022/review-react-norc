e9a0a403a88472c1f17ddfaa2e8419d68c071b23
dsl2055 <job2019tmm@gmail.com>
2022-08-07 13:59:36 -0400

    updated

--- src/Components/Notes/NoteCard/index.js
+++ src/Components/Notes/NoteCard/index.js
@@ -1,6 +1,12 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { useContext } from "react";
+import { NotesContext } from "../../../Context/NoteContext";
 export default function Note({ title, description, createdAt, priority }) {
+  const { setisDetailShowed, isDetailShowed } = useContext(NotesContext);
+  function handleModalOpen() {
+    setisDetailShowed(true);
+  }
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Body>
@@ -8,7 +14,9 @@ export default function Note({ title, description, createdAt, priority }) {
         <Card.Text>{createdAt}</Card.Text>
         <Card.Text>{priority}</Card.Text>
         <Card.Text>{description}</Card.Text>
-        <Button variant="primary">View Detail</Button>
+        <Button onClick={handleModalOpen} variant="primary">
+          View Detail
+        </Button>
       </Card.Body>
     </Card>
   );
