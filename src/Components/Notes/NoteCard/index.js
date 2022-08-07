a6282474ca6cd074eebd04ca6b6cdee280511525
dsl2055 <job2019tmm@gmail.com>
2022-08-07 13:41:59 -0400

    styles completed

--- src/Components/Notes/NoteCard/index.js
+++ src/Components/Notes/NoteCard/index.js
@@ -3,7 +3,6 @@ import Card from "react-bootstrap/Card";
 export default function Note({ title, description, createdAt, priority }) {
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{createdAt}</Card.Text>
--- src/Components/Notes/index.js
+++ src/Components/Notes/index.js
@@ -1,20 +1,28 @@
 import { useContext } from "react";
 import { NotesContext } from "../../Context/NoteContext";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Stack from "react-bootstrap/Stack";
 import Note from "./NoteCard/index";
 export default function Notes() {
   const { notes, setNotes, setCurrentNote } = useContext(NotesContext);
   console.log(notes);
   return (
-    <>
-      {notes.map(({ id, title, description, createdAt, priority }, index) => (
-        <Note
-          key={index}
-          title={title}
-          description={description}
-          createdAt={createdAt}
-          priority={priority}
-        />
-      ))}
-    </>
+    <Container>
+      <Row>
+        {notes.map(({ id, title, description, createdAt, priority }, index) => (
+          <Col sm={6} style={{ marginTop: 20 }}>
+            <Note
+              key={index}
+              title={title}
+              description={description}
+              createdAt={createdAt}
+              priority={priority}
+            />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
